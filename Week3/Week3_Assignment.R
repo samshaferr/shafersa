7e7d0dda8c2d3f1d22a6487708125332fc9a5980
samshaferr <shafer@susqu.edu>
2024-09-16 13:54:07 -0400

    New edits but right this time. I was having trouble with simpsons

--- Week3/Week3_Assignment.R
+++ Week3/Week3_Assignment.R
@@ -15,31 +15,6 @@
 
     # Copy-paste into your script - and run - an example from the reference manual for a function within this package related to a measure of genetic drift. 
 
-Drift_graph = function(t,R){
-  N<-250
-  p<-0.5
-  freq<-as.numeric();
-  for( i in 1:t ){
-    A1=rbinom(1,2*N,p)
-    p=A1/(N*2);
-    freq[length(freq)+1]<-p;
-  }
-  plot(freq,type="l",ylim=c(0,1),col=3,xlab="t",ylab=expression(p(A[1])))
-  for(u in 1:R){
-    freq1<-as.numeric();
-    p<-0.5
-    for( j in 1:t ){
-      A1=rbinom(1,2*N,p)
-      p=A1/(N*2);
-      freq1[length(freq1)+1]<-p;
-    }
-    random<-sample(1:1000,1,replace=F)
-    randomcolor<-colors()[random] 
-    lines(freq1,type="l",col=(randomcolor))
-  }
-}
-
-Drift_graph(2000,50)
 
 # Pop   = Replicate populations
 # Gen   = Generations
@@ -84,22 +59,22 @@ GenDriftSim = function(Pop = Pop, Gen = Gen, NM, NF, P, graph = "y", histo = "y"
   }else{}
 }
 
-Pop = 20
+Pop = 10
 Gen = 25
 P = 0.5
 
 SimResults = matrix(data = NA, ncol = 3, nrow = Gen*Pop)
 GenDriftSim(Pop = Pop, Gen = Gen, NM = 100, NF = 900, P = P, graph = "y",  histo = "n")
-GenDriftSim(Pop = Pop, Gen = Gen, NM = 180, NF = 180, P = P, graph = "y",  histo = "n")
-?dev.off
+dev.off
 
+##The codw that I found online shows an imbalance between the male and female population size 
 
         # Depending on the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
-##This is from the second line if code that I ran from the GenDriftSim. I adjusted the population size
+
  # After running the function example, manipulate a parameter within the function to create a new result. 
 
 GenDriftSim = function(Pop = Pop, Gen = Gen, NM, NF, P, graph = "y", histo = "y"){
-  P = (2*(NM+NF))*P
+  P = (2.1*(NM+NF))*P
   NE = round((4*NM*NF)/(NM+NF),0)
   SR = round(NM/NF,2)
   Na = NM+NF
@@ -138,6 +113,12 @@ GenDriftSim = function(Pop = Pop, Gen = Gen, NM, NF, P, graph = "y", histo = "y"
 Pop = 20
 Gen = 25
 P = 0.5
+
+SimResults = matrix(data = NA, ncol = 3, nrow = Gen*Pop)
+GenDriftSim(Pop = Pop, Gen = Gen, NM = 180, NF = 180, P = P, graph = "y",  histo = "n")
+dev.off
+
+##This new code shows if the male and female populations were equal in size.
         
 # Common options might be allele frequency, population size, fitness level, etc. 
         # Add the results of this manipulation to your script (if in the console) or upload the new plot.
@@ -150,6 +131,26 @@ P = 0.5
 ## (4) Using Google and ONLY packages from GitHub or CRAN:
     # Find a package that will generate standard diversity metrics for community ecology, specifically Simpson's Diversity Index.
     # Copy-paste into your script - and run - an example from the reference manual for a function to calculate Simpson's diversity. 
+Abundancia2017<- c(0,36,0,9,0,0,46,0,0,0,25,0)
+Abundancia2018<- c(0,46,0,13,5,0,69,2,0,0,123,9)
+Abundancia2019<- c(4,20,0,38,2,1,97,0,0,0,12,0)
+datafish <- data.frame(Abundancia2017,Abundancia2018, Abundancia2019)
+
+require(vegan)
+y <- for(i in 1:nrow(df)) {
+  row <- df[i,4:50] 
+  #Assuming 50 columns
+  diversity(row, "simp")
+}
+
+library("dplyr")
+df <- data.matrix(datafish) %>%
+  t() %>%
+  as.data.frame()
+
+diversity(df,index = "simpson")
+
+
         # Depending on the example usage of the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
     # After running the function example, modify your script to generate another diversity metric that is NOT part of the example. 
         # If there are multiple diversity metrics in the example script, none of these will count as the modified script.
