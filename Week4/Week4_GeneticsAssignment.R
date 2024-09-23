36be323359b6e846be9284f61f44ae87c0d33d20
samshaferr <shafer@susqu.edu>
2024-09-23 13:27:40 -0400

    HEYYYY GIRLIE

--- Week4/Week4_GeneticsAssignment.R
+++ Week4/Week4_GeneticsAssignment.R
@@ -1,15 +1,32 @@
 # Look at the plot and model results for our Dryad data in the tutorial. 
   # Part 1: Without knowing which points represent which groups,give one explanation for why these data might be difficult
   # to draw spatial inferences about genes.(4 points)
-  # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (4 points), and EXPLAIN WHY (6 points).
+##One way is might be hard to make spatial inferences about these genes is because we do not know there methods for sampling. It could be sampled in clumps and not randomly sampled. This could lead to bias and inaccurate results
 
+  # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (4 points), and EXPLAIN WHY (6 points).
+##After fully running the data and color coding by region it s clear there methods for sampling were skewed. 
+##When analyzing the literature directly from the Dryad article they said that the data they collected was randomly. 
+##I think what they were possibly trying to do was to collect the most diverse set of data that they could. 
+##But when the data is represented graphically it suggests that samples were collected in clusters. 
+##The data is also missing samples that are below a latitude of 0. It could also be an issue of the data misrepresenting the literature. 
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
-  # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
 
+  # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
+install.packages('stability')
+library(stability)
+data("ge_data")
+head(ge_data)
 # Create two linear models for Yield Response: one related to the Environment and one to the Genotype. (2 points each)
   # 'Yield Response' in this dataset is a measure of phenotype expression.
   # Hint: Look at the help file for this dataset.
+help(ge_data)
+help("stability")
+hist(ge_data)
+colnames(ge_data)
+row.names(ge_data)
+plot(data$Gen ~ data$Yield)
+
 
 # Test the significance of both models and look at the model summary. (4 points each)
   # Which model is a better fit to explain the yield response, and WHY? (6 points)
