c872e6abe73450c9d34c2f8eb022b7f8988adb5e
samshaferr <shafer@susqu.edu>
2024-10-07 14:32:52 -0400

    New Edits

--- Week6/Week6_MixedModel_Assignment.R
+++ Week6/Week6_MixedModel_Assignment.R
@@ -27,12 +27,19 @@ summary(glmm.mod2)
 df$prop.cons <- df$eaten/df$prey 
 
 # (Q1) - The code I've provided in line 13 above is performing two operations at once. What are they? (2 pts)
-## prop.cons represent the proportion of mussels consumed
+## By dividing what has been eaten by prey it creates a rate. The result of the rate is demonstrated in the new row produced in the data frame. 
+## You are doing a division operation as well as creating an output for said function and adding that to the data frame. 
 
 # (Q2) - Did the interactive effect change which variables predict proportional consumption? How, SPECIFICALLY, did the results change? (5 pts)
 
+##Yes, the interactive effect changed with predicted proportional consumption. Generally as crab activity increased, the proportion of mussels 
+## consumed also increased. this was the case with smaller crabs but had no effect on the mussel consumption in lager crabs.
+
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
+## I think the first model is a better fit for the data. There is less clumping of the points and more even distribution of 
+## points. Whereas in the second model the points generally get more dense as you move toward the bottom right corner of the graph. 
+## There are clearly some outliers in both models but the first model has less.
 
 plot(glmm.mod1)
 plot(glmm.mod2)
@@ -45,11 +52,11 @@ summary(gam.mod1)
 gam.mod2<-gam(eaten/prey~temperature+carapace.width*claw.width, family=binomial, random = ~ 1 | block, data = df)
 summary(gam.mod2)
 # (Q4) - Which model is a better fit? (2 pt)
-
+##gam.mod2
 
 # (Q5) - Based on the residuals of your generalized additive models, how confident are you in these results? (2 pts)
 
-
+##Neither result gave way to much significance. They both didn't have a strong R value. 
 
 
 
