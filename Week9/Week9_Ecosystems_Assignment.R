08a77388a5aeabe61aad9ed06a09c6001bdcfe66
samshaferr <shafer@susqu.edu>
2024-10-29 08:32:17 -0400

    new edits

--- Week9/Week9_Ecosystems_Assignment.R
+++ Week9/Week9_Ecosystems_Assignment.R
@@ -25,7 +25,7 @@ abiotic.means1 <- abiotic.means[,-16]
 abiotic.means2 <- abiotic.means1[,-1:-6] 
 abiotic.means2 <- sapply(abiotic.means2, as.numeric ) 
 abiotic.means2 <- as.data.frame(abiotic.means2)
-abiotic.means <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
+abiotic.means2 <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
 
 veg_trans$names <- paste(veg_trans$Parcel, veg_trans$Landuse)
 veg_trans.means <- aggregate(x = veg_trans, by = list(veg_trans$names), FUN = "mean")
@@ -44,14 +44,53 @@ ncol((abiotic.means2))
 
 ord <- rda(veg_trans.means3 ~ pH + totalN + Perc_ash + Kalium + Magnesium + Ca + Al + TotalP + OlsenP, abiotic.means2)
 ord
-##Just over half of variance is explained (54%).
 anova(ord)
 plot(ord, ylim = c(-7,7), xlim = c(-7,7))  
+ord2 <- rda(veg_trans.means3 ~ pH + totalN + Ca + TotalP + OlsenP, abiotic.means2)
+ord2
+anova(ord2)
+plot(ord2, ylim = c(-7,7), xlim = c(-7,7))  
+
+#Examples of different combinations that I had tried
+#ord2 <- rda(veg_trans.means3 ~ pH , abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ toatlN, abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ Perc_ash , abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ Kalium , abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ Ca, abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ Al , abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ TotalP , abiotic.means2)
+#ord2 <- rda(veg_trans.means3 ~ OlsenP , abiotic.means2)
+
+veg_trans.means3 <- na.omit(veg_trans.means3)
+abiotic.means3 <- na.omit(abiotic.means2)
+
+ord <- rda(veg_trans.means3 ~., abiotic.means2) 
+ord.int <- rda(veg_trans.means3 ~1, abiotic.means2)
+step.mod <- ordistep(ord.int, scope = formula(ord), selection = "both")
+step.mod$anova
+
+##Just over half of variance is explained (54%) with the 1st RDA with all of the abiotic variables included. After I ran the ANOVA the results can back insignificant.I then tried a series of different combination of
+##variables to see if it was telling a different story when they were separate. This in fact did not lead to any Significance. The variables that I did chose to run, I thought were the most influential for vegetation growth. 
+##
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
-  # Explain the ecological importance of the significant predictors, or lack of significant predictors.
+# Explain the ecological importance of the significant predictors, or lack of significant predictors.
+library(fitdistrplus)
+library(logspline)
 
+abiotic.means2$Parcel <- unique(abiotic$Parcel)
+merged <- merge(abiotic, veg_trans, by = "Parcel")
+merged$Land_Use <- as.numeric(merged$Land_Use)
 
+fit.weibull <- fitdist(merged$Land_Use, distr = "weibull")
+fit.norm <- fitdist(merged$totalN, distr = "norm")
+fit.gamma <- fitdist(merged$totalN, distr = "gamma")
+fit.lnorm <- fitdist(merged$totalN, distr = "lnorm")
+fit.logis <- fitdist(merged$totalN, distr = "logis")
+gofstat(list(fit.weibull, fit.norm, fit.gamma, 
+             fit.lnorm, fit.logis))
+
+mod1 <- lm(merged$Land_Use~totalN + Kalium + Magnesium + Ca + Al + TotalP )
 
 # (Q3 - 6 pts) Provide a 3-4 sentence synthesis of how these results relate to one another and the value of considering both together for interpreting biotic-abiotic interactions.
 
