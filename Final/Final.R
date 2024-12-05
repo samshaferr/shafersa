e448ccee1bbeb00b109acf9265ab9817c4e7a1f0
samshaferr <shafer@susqu.edu>
2024-12-05 09:49:40 -0500

    poop

--- Final/Final.R
+++ Final/Final.R
@@ -3,15 +3,24 @@ getwd()
 setwd("C:/GitHub/shafersa/Final")
 
 library(readxl)
-CASTfile <- read_excel("CASTfile.xlsx")
-View(CASTfile)
+CastScalingdown <- read_excel("CastScalingdown.xlsx", 
+                              sheet = "Major Source - Fed vs NonFed")
+View(CastScalingdown)
+colnames(CastScalingdown)
+CleanCAST <- CastScalingdown[ -(12:22), -(27:36) ]
+
 
 library(readr)
 Chesapeake_Bay_Pollution_Loads_Sediment_20241121 <- read_csv("Chesapeake_Bay_Pollution_Loads_-_Sediment_20241121.csv")
 View(Chesapeake_Bay_Pollution_Loads_Sediment_20241121)
 
 
+
+CastScalingdown<-sapply(strsplit(CastScalingdown$Geography, ', '), "[[", 1)
+strsplit(CastScalingdown$Geography, ', ')
+
 ?merge
-merged<-merge(CASTfile, Chesapeake_Bay_Pollution_Loads_Sediment_20241121,
-      by.x = "Sector" , by.y = "Source Sector" , all = FALSE)
+merged<-merge(CastScalingdown, Chesapeake_Bay_Pollution_Loads_Sediment_20241121,
+      by.x = "Geography" , by.y = "County" , all = FALSE)
+
 
